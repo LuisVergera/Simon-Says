df7282b1fa8c63c99e085a981ff6828b67a40d13
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-03-24 21:43:24 -0300

    Agrego archivos iniciales y parte visual resuelta

---
+++ Index.js
@@ -0,0 +1,32 @@
+const green = document.querySelector('#colour-green');
+const yellow = document.querySelector('#colour-yellow');
+const red = document.querySelector('#colour-red');
+const blue = document.querySelector('#colour-blue');
+
+const sequence = [green, yellow, red, blue]
+
+
+
+let simonTurn = [];
+
+function simonSays(){
+    for (let i = 0; i<)
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
