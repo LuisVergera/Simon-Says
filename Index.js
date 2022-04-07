f666cc3c1a21fd683c2f1af62784128e8d94094a
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-04-07 19:34:56 -0300

    Añado funcion para que se registre el input del jugador

--- Index.js
+++ Index.js
@@ -6,6 +6,9 @@ const blue = document.querySelector("#colour-blue");
 const sequence = [green, yellow, red, blue];
 const randomSequence = sequence[Math.floor(Math.random() * sequence.length)];
 
+let simonSequence = [];
+let playerSequence = [];
+
 function flash(colour) {
   colour.classList.add("active");
 }
@@ -17,3 +20,33 @@ function flashRandomColour() {
     }, 1500);
   });
 }
+
+/*
+function simonTurn() {
+  let timesFlashed = 0;
+
+  if (simonSequence === playerSequence) {
+    for (let i = 1; i < 20; i++) {}
+  }
+}
+*/
+
+function playerTurnHandler() {
+  document.getElementById("colour-green").onclick = function () {
+    playerSequence.push(0);
+  };
+
+  document.getElementById("colour-yellow").onclick = function () {
+    playerSequence.push(1);
+  };
+
+  document.getElementById("colour-red").onclick = function () {
+    playerSequence.push(2);
+  };
+
+  document.getElementById("colour-blue").onclick = function () {
+    playerSequence.push(3);
+  };
+}
+
+playerTurnHandler();
