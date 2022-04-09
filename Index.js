342c0fda959569cdac2a05fda74e8aaeb0d8a817
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-04-09 11:34:23 -0300

    Agrego cambios a colores y funcion

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
-const green = document.querySelector("#colour-green");
-const yellow = document.querySelector("#colour-yellow");
-const red = document.querySelector("#colour-red");
-const blue = document.querySelector("#colour-blue");
+const green = document.querySelector("#0");
+const yellow = document.querySelector("#1");
+const red = document.querySelector("#2");
+const blue = document.querySelector("#3");
 
 const sequence = [green, yellow, red, blue];
 const randomSequence = sequence[Math.floor(Math.random() * sequence.length)];
@@ -11,6 +11,9 @@ let playerSequence = [];
 
 function flash(colour) {
   colour.classList.add("active");
+  setTimeout(function () {
+    colour.classList.remove("active");
+  }, 250);
 }
 
 function flashRandomColour() {
@@ -21,32 +24,35 @@ function flashRandomColour() {
   });
 }
 
-/*
-function simonTurn() {
-  let timesFlashed = 0;
+function round() {}
 
-  if (simonSequence === playerSequence) {
-    for (let i = 1; i < 20; i++) {}
-  }
+function simonTurn() {
+  playerSequence = [];
+  compareSequenceArrays();
 }
-*/
 
 function playerTurnHandler() {
-  document.getElementById("colour-green").onclick = function () {
+  document.getElementById("0").onclick = function () {
     playerSequence.push(0);
   };
 
-  document.getElementById("colour-yellow").onclick = function () {
+  document.getElementById("1").onclick = function () {
     playerSequence.push(1);
   };
 
-  document.getElementById("colour-red").onclick = function () {
+  document.getElementById("2").onclick = function () {
     playerSequence.push(2);
   };
 
-  document.getElementById("colour-blue").onclick = function () {
+  document.getElementById("3").onclick = function () {
     playerSequence.push(3);
   };
 }
 
 playerTurnHandler();
+
+function compareSequenceArrays(playerSequence, simonSequence) {
+  if (playerSequence != simonSequence) {
+    gameover();
+  }
+}
